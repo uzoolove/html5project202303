3fdd117f54bfefea9b548e8742648e03bd30cc5c
kilyong jeong <uzoolove@gmail.com>
2023-03-20 14:54:42 +0900

    ejs 파일 추가

--- workspace-ins/mulpang/app.js
+++ workspace-ins/mulpang/app.js
@@ -13,22 +13,24 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// 미들웨어
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(logger('dev'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, req.url + ' Not Found'));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.error(err.stack);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- workspace-ins/mulpang/routes/index.js
+++ workspace-ins/mulpang/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('today.html');
 });
+
+router.get('/:page.html', function(req, res, next) {
+  res.render(req.params.page);
+});
+
+
 
 module.exports = router;
