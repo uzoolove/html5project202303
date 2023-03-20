2d6588200dff6edeb4deede819cc26de5fc38ebc
kilyong jeong <uzoolove@gmail.com>
2023-03-20 11:41:07 +0900

    웹서버 동기방식 호출까지

---
+++ workspace-ins/nodeweb/webserver.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const home = path.join(__dirname, 'html');
+
+const server = http.createServer(function(req, res){
+  console.log(req.method, req.url, req.httpVersion);
+  console.log(req.headers);
+
+  var filename = req.url;
+
+  try{
+    var data = fs.readFileSync(path.join(home, filename));
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    res.end(data);
+  }catch(err){
+    res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'});
+    res.end('<h1>' + filename + ' 파일을 찾을 수 없습니다.</h1>');
+  }
+});
+
+server.listen(1234, function(){
+  console.log('HTTP 서버 구동 완료.');
+});
