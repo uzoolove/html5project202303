6ed6ab6d9a7d5186172bf8748b8cac736a948c6f
kilyong jeong <uzoolove@gmail.com>
2023-03-21 10:56:50 +0900

    쿠폰 상세조회

--- workspace-ins/mulpang/routes/index.js
+++ workspace-ins/mulpang/routes/index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const model = require('../model/mulpangDao');
+const MyUtil = require('../utils/myutil');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('today.html');
+  res.redirect('today');
+});
+
+// 오늘 메뉴
+router.get('/today', async function(req, res, next) {
+  var list = await model.couponList();
+  res.render('today', { list });
+});
+
+// 쿠폰 상세 조회
+router.get('/coupons/:_id', async function(req, res, next) {
+  var coupon = await model.couponDetail(Number(req.params._id));
+  res.render('detail', { coupon, toStar: MyUtil.toStar });
 });
 
 router.get('/:page.html', function(req, res, next) {
   res.render(req.params.page);
 });
-
 
 
 module.exports = router;
